734870da132d51414441110442b1a45688db8a57
throne404 <56145871+throne404@users.noreply.github.com>
2020-02-13 21:36:06 +0700

    Add files via upload

---
+++ adfin2.php
@@ -0,0 +1,51 @@
+<?php
+// Author Name : ./Throne404
+// Information List : 4001 list
+// greetz : Crash system squad
+
+print "   
+	
+ █████╗ ██████╗ ███╗   ███╗██╗███╗   ██╗    ███████╗██╗███╗   ██╗██████╗ ███████╗██████╗ 
+██╔══██╗██╔══██╗████╗ ████║██║████╗  ██║    ██╔════╝██║████╗  ██║██╔══██╗██╔════╝██╔══██╗
+███████║██║  ██║██╔████╔██║██║██╔██╗ ██║    █████╗  ██║██╔██╗ ██║██║  ██║█████╗  ██████╔╝
+██╔══██║██║  ██║██║╚██╔╝██║██║██║╚██╗██║    ██╔══╝  ██║██║╚██╗██║██║  ██║██╔══╝  ██╔══██╗
+██║  ██║██████╔╝██║ ╚═╝ ██║██║██║ ╚████║    ██║     ██║██║ ╚████║██████╔╝███████╗██║  ██║
+╚═╝  ╚═╝╚═════╝ ╚═╝     ╚═╝╚═╝╚═╝  ╚═══╝    ╚═╝     ╚═╝╚═╝  ╚═══╝╚═════╝ ╚══════╝╚═╝  ╚═╝
+                                                                                         
+                Admin Finder - Recoded by ./Throne404
+  Information list : 4001 list
+  Thanks to  : Crash system squad
+";
+
+echo "enter site  : ";
+$target = trim(fgets(STDIN));
+$list = "throne.txt";
+if(!preg_match("/^http:\/\//",$target) AND !preg_match("/^https:\/\//",$target)){
+	$targetnya = "http://$target";
+}else{
+	$targetnya = $target;
+}
+
+$buka = fopen("$list","r");
+$ukuran = filesize("$list");
+$baca = fread($buka,$ukuran);
+$lists = explode("\r\n",$baca);
+
+foreach($lists as $login){
+	$log = "$targetnya/$login";
+	$ch = curl_init("$log");
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_exec($ch);
+	$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+	curl_close($ch);
+	if($httpcode == 200){
+		 $handle = fopen("result.txt", "a+");
+		fwrite($handle, "$log\n");
+		print "\n\n [".date('H:m:s')."] Trying : $log => found\n";
+	}else{
+		print "\n[".date('H:m:s')."] Trying : $log => not found";
+	}
+}
+  
+?>
